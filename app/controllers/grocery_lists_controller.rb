da65e223a8aa932503c2553cf5ad7d2bae2ba31d
gregdardis <dardis.greg@gmail.com>
2018-04-03 15:53:27 -0700

    created grocery_lists controller, defined actions outline in comments, added resource to routes.rb

---
+++ app/controllers/grocery_lists_controller.rb
@@ -0,0 +1,26 @@
+class GroceryListsController < ApplicationController
+  # (GET) get all grocery lists
+  # PATH: /grocery_lists
+  # def index
+  # end
+
+  # (POST) create new grocery list
+  # PATH: /grocery_lists
+  # def create
+  # end
+
+  # (GET) get specific grocery list by id
+  # PATH: /grocery_lists/:id
+  # def show
+  # end
+
+  # (PATCH) update specific grocery list by id
+  # PATH: /grocery_lists/:id
+  # def update
+  # end
+
+  # (DELETE) delete specific grocery list by id
+  # PATH: /grocery_lists/:id
+  # def destroy
+  # end
+end
