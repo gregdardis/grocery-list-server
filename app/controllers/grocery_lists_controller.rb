f9901e0fbfaf49d2c9b80c92b3228fc1e5374c59
gregdardis <dardis.greg@gmail.com>
2018-04-03 19:32:38 -0700

    fixed database, uncommented actions in grocery_lists_controller

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,26 +1,26 @@
 class GroceryListsController < ApplicationController
   # (GET) get all grocery lists
   # PATH: /grocery_lists
-  # def index
-  # end
+  def index
+  end
 
   # (POST) create new grocery list
   # PATH: /grocery_lists
-  # def create
-  # end
+  def create
+  end
 
   # (GET) get specific grocery list by id
   # PATH: /grocery_lists/:id
-  # def show
-  # end
+  def show
+  end
 
   # (PATCH) update specific grocery list by id
   # PATH: /grocery_lists/:id
-  # def update
-  # end
+  def update
+  end
 
   # (DELETE) delete specific grocery list by id
   # PATH: /grocery_lists/:id
-  # def destroy
-  # end
+  def destroy
+  end
 end
---
+++ db/migrate/20180403230813_create_grocery_lists.rb
@@ -0,0 +1,14 @@
+class CreateGroceryLists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :grocery_lists do |t|
+      # table has an ID by default
+      t.string :title
+      t.string :owner
+      t.string :last_modified_by
+      t.timestamp :created_at
+      t.timestamp :last_modified
+
+      t.timestamps
+    end
+  end
+end
