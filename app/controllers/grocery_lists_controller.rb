ca1a46339836035c77e6153301bb8c4d54fe3ca5
gregdardis <dardis.greg@gmail.com>
2018-04-04 10:34:23 -0700

    create new grocery list

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,12 +1,27 @@
 class GroceryListsController < ApplicationController
+
   # (GET) get all grocery lists
   # PATH: /grocery_lists
   def index
+    # @grocery_lists = GroceryList.all
+
+    # render status: :ok, json: @grocery_lists.to_json
   end
 
   # (POST) create new grocery list
   # PATH: /grocery_lists
   def create
+    # initializes a GroceryList with attributes automatically
+    # mapped to the respective database columns
+    @grocery_list = GroceryList.new(grocery_list_params)
+
+    # saves the model in the database
+    @grocery_list.save
+
+    # sends back status 200 (ok) with the grocery list as json
+    render status: :ok
+    # render status: :ok, json: @grocery_list.to_json
+    # render json: params
   end
 
   # (GET) get specific grocery list by id
@@ -22,5 +37,16 @@ class GroceryListsController < ApplicationController
   # (DELETE) delete specific grocery list by id
   # PATH: /grocery_lists/:id
   def destroy
+  end
+
+  private
+  def grocery_list_params
+    params.require(:grocery_list).permit(:title,
+                                          :owner,
+                                          :last_modified_by,
+                                          :created_at,
+                                          :last_modified,
+                                          :updated_at
+                                        )
   end
 end
